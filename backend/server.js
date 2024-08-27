81dc2028b2743aa6815c669e69aaee468878dfbb
Sunday favour <90037865+Sunday012@users.noreply.github.com>
2024-08-27 05:57:20 +0100

    fixed backend issues

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express')
 const cors = require('cors');
 const bodyParser = require('body-parser')
-const projectRoutes = require("./api/routes")
 const pool = require('./db')
 const app = express();
 
