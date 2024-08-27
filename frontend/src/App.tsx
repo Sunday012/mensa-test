2825bd698f91e01d76e00cd0e9e53d58c177e03b
Sunday favour <90037865+Sunday012@users.noreply.github.com>
2024-08-27 06:17:10 +0100

    added dist to my vite config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,24 @@
-
-import './App.scss'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.scss';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Layout } from 'antd';
 import HomePage from './pages/home';
 import ProjectDetails from './pages/projectDetails';
-const { Content } = Layout;
-function App() {
 
+const { Content } = Layout;
+
+function App() {
   return (
     <Router>
       <Layout>
         <Content style={{ padding: '50px' }}>
+          <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/project/:id" element={<ProjectDetails />} />
+          </Routes>
         </Content>
       </Layout>
     </Router>
-  )
+  );
 }
 
-export default App
-
+export default App;
