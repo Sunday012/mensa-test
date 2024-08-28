59951a6e1b4b27c212dcfeaf8ea1722251ff8e3e
Sunday favour <90037865+Sunday012@users.noreply.github.com>
2024-08-28 09:33:47 +0100

    fiexd routing issues

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,44 +1,59 @@
-
-import '../App.scss'
+import '../App.scss';
 import ListProjects from '../components/ListProjects';
 import { CreateProjects } from '../components/CreateProjects';
 import { useEffect, useState } from 'react';
 import { fetchProjects } from '../services/api';
 import Cookies from 'js-cookie';
-// import { useSelector } from 'react-redux';
-// import { RootState } from '../redux/store';
 
 type ProjectProps = {
-        id: number,
-        name: string,
-        description: string,
-        due_date?: string,
-        user_id?: string,
-}
+    id: number;
+    name: string;
+    description: string;
+    due_date?: string;
+    user_id?: string;
+};
+
 function HomePage() {
-    const [projects, setProjects] = useState<ProjectProps[]>([])
-    const token = Cookies.get('token')
+    const [projects, setProjects] = useState<ProjectProps[]>([]);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    const token = Cookies.get('token');
+
     useEffect(() => {
-        const fetchAllProjects = async () => {
-            const result = await fetchProjects();
-            setProjects(result);
-            console.log(result);
-        }
+        const checkAuthAndFetchProjects = async () => {
+            if (token) {
+                try {
+                    const response = await fetchProjects();
+                    if (response) {
+                        setIsAuthenticated(true);
+                        setProjects(response);
+                    } else {
+                        setIsAuthenticated(false);
+                    }
+                } catch (error) {
+                    console.error("Authentication failed or token is invalid:", error);
+                    setIsAuthenticated(false);
+                }
+            } else {
+                setIsAuthenticated(false);
+            }
+        };
 
-        if(token){
-            fetchAllProjects()
-        }
-    },[])
+        checkAuthAndFetchProjects();
+    }, [token]);
 
-  return (
-    <>
-    <div className=''>
-      <CreateProjects />
-      <ListProjects projects={projects} />
-    </div>
-    </>
-  )
+    return (
+        <div>
+            {isAuthenticated ? (
+                <>
+                    <CreateProjects />
+                    <ListProjects projects={projects} />
+                </>
+            ) : (
+                <p>Please log in to view your projects.</p>
+            )}
+        </div>
+    );
 }
 
-export default HomePage
-
+export default HomePage;
